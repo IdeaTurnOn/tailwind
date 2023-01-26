8aa7b8438a66d5c0c2a1be77a68e080e5894dd1c
Nikalanda Bandara <chandima96dilan@gmail.com>
2023-01-26 23:22:00 +0530

    all pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'torenge': '#FFA43A',
+        'tyel-1': '#FFC637',
+        'tyel-2': '#FFD15F',
+        'tyel-3': '#FFE9B2',
+      },
+      fontFamily: {
+        'pop': ['Poppins', 'sans-serif'],
+    },
+    },
   },
   plugins: [],
 }
